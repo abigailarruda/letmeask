a57014c37dd0843c0f623f2d67348c2b0470d8cb
Abigail <itssabbie@gmail.com>
2021-06-25 14:46:09 -0300

    surface exploration

--- letmeask/src/components/Question.tsx
+++ letmeask/src/components/Question.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
+import cx from "classnames";
+
 import "../assets/styles/question.scss";
 
 type QuestionProps = {
@@ -9,18 +11,32 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHightlighted?: boolean;
 };
 
-export default function Question({ content, author, children }: QuestionProps) {
+export default function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHightlighted = false,
+  children,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHightlighted && !isAnswered }
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div>{children}</div>
+        <div className="buttons">{children}</div>
       </footer>
     </div>
   );
--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -10,6 +10,8 @@ import Question from "../components/Question";
 
 import logoImage from "../assets/images/logo.svg";
 import deleteImage from "../assets/images/delete.svg";
+import checkImage from "../assets/images/check.svg";
+import answerImage from "../assets/images/answer.svg";
 
 import "../assets/styles/room.scss";
 
@@ -30,6 +32,18 @@ export default function AdminRoom() {
     if (window.confirm("Tem certeza que você deseja excluir essa pergunta?")) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlighQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHightlighted: true,
+    });
   }
 
   async function handleEndRoom() {
@@ -72,7 +86,30 @@ export default function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isHightlighted={question.isHightlighted}
+                isAnswered={question.isAnswered}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImage}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+
+                    <button
+                      type="button"
+                      onClick={() => handleHighlighQuestion(question.id)}
+                    >
+                      <img src={answerImage} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
+
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
